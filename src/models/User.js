bcf61ee8abd6a4a4b5921c077b025684f2a7d54e
Raj patil <sghitech29@gmail.com>
2026-05-20 11:17:45 +0530

    auth

--- src/models/User.js
+++ src/models/User.js
@@ -46,16 +46,9 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Hash password before saving
-userSchema.pre('save', async function () {
-  if (!this.isModified('password')) return;
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
-
-// Compare password
+// Compare password in plain text
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+  return enteredPassword === this.password;
 };
 
 module.exports = mongoose.model('User', userSchema);
